5bdd8e6b515b0c289357bb1b3dcdd53003845cc0
guymoyo <guymoyo@yahoo.fr>
2013-10-29 17:51:40 +0100

    d

---
+++ src/main/java/com/nomade/service/LoginPathInterceptor.java
@@ -0,0 +1,22 @@
+package com.nomade.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.nomade.domain.BeanRegister;
+
+
+public class LoginPathInterceptor extends HandlerInterceptorAdapter {
+	@Override
+    public boolean preHandle(HttpServletRequest request,
+    HttpServletResponse response, Object handler) throws Exception {
+        String requestUrl = request.getRequestURI().substring(request.getContextPath().length());
+        if ("/login".equals(requestUrl)) {
+        	request.setAttribute("beanRegister", new BeanRegister());
+        }
+        	
+        return true;        
+    }
+
+}
