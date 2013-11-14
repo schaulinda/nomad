c86e011ecaa293f45daa94e6963715c010dce127
guymoyo <guymoyo@yahoo.fr>
2013-11-14 22:51:41 +0100

    .

---
+++ src/main/java/com/nomade/tools/ImageUtilInterface.java
@@ -0,0 +1,9 @@
+package com.nomade.tools;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageUtilInterface {
+	
+	public boolean isValidate(MultipartFile file);
+
+}
