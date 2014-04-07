7c5bb0a7a675b147de1dd353da7159d4ac73e945
guymoyo <guymoyo@yahoo.fr>
2014-04-08 00:04:18 +0100

    .

--- src/main/java/com/nomade/web/AlbumController.java
+++ src/main/java/com/nomade/web/AlbumController.java
@@ -53,6 +53,7 @@ public class AlbumController {
 		String idAlbum;
 		List<String> listIdPhoto;
 		List<Album> albums = nomade.getAlbums();
+		
 		if (albums != null && albums.size() > 0) {
 
 			Album album = albums.iterator().next();
